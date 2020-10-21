aa60506fd9faf69481987ec34a936f5e781eb584
Stephan Venter <gs.venter@gmail.com>
2020-10-21 16:49:20 +1300

    Tabbed Nav added and map added

--- CoffeeShop/CoffeeShop/App.xaml.cs
+++ CoffeeShop/CoffeeShop/App.xaml.cs
@@ -10,7 +10,7 @@ namespace CoffeeShop
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
+            MainPage = new NavigationPage(new MainTabbedPage());
 
         }
 
