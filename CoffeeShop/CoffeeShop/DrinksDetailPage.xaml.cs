5cd90b187b64d98bf419dfb35843dc9ec9c02f66
Stephan Venter <gs.venter@gmail.com>
2020-10-21 15:53:14 +1300

    First commit

---
+++ CoffeeShop/CoffeeShop/DrinksDetailPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CoffeeShop
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DrinksDetailPage : ContentPage
+    {
+        public DrinksDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void backButton_Clicked(object sender, EventArgs e)
+        {
+            //await Navigation.PopAsync();
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
